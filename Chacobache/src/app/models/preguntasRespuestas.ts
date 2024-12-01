4cf1924b4178777fd78ce78454be2a014ac931df
pcerezo <pcerezo70@gmail.com>
2024-12-01 18:35:54 +0100

    Clean gh-pages

--- Chacobache/src/app/models/preguntasRespuestas.ts
+++
@@ -1,9 +0,0 @@
-export class PreguntasRespuestas {
-    constructor(
-        public id: number,
-        public asunto: string,
-        public texto_pregunta: string,
-        public texto_respuesta: string,
-        public fecha_publicacion: Date
-    ) {}
-}
