530b035587e934623e9189d81433430a503ff9ac
pcerezo <pcerezo70@gmail.com>
2024-12-05 08:36:11 +0100

    Probando a añadir cambios de cors y correcciones en docker-compose para subir contenedores

--- Chacobache_API/index.js
+++ Chacobache_API/index.js
@@ -5,6 +5,11 @@ require('dotenv').config();
 
 const app = express();
 app.use(cors());
+/*
+app.use(cors({
+  origin: 'https://pcerezo.github.io', // Tu frontend en GitHub Pages
+}));
+*/
 app.use(express.json());
 
 // Rutas de eventos
